e567f2a9555804de7cef836b1571543923d41916
proudhAteR <158060444+proudhAteR@users.noreply.github.com>
2024-10-24 14:17:50 -0400

    feature: Ongoing class validation implemented

--- src/Course.ino
+++ src/Course.ino
@@ -8,13 +8,33 @@ class Course
     const char *name;
     int weekDay;
     int startingHour;
+    int endHour;
 
 public:
-    Course(const char *name,int weekday, int startingHour)
+    Course(const char *name, int startingHour, int endHour, int weekday)
     {
         this->name = name;
         this->weekDay = weekDay;
         this->startingHour = startingHour;
+    }
+
+    public :
+    int getStartingHour(){
+        return this-> startingHour;
+    }
+    public :
+    int getEndHour(){
+        return this-> endHour;
+    }
+
+    public :
+    int getWeekDay(){
+        return this-> weekDay;
+    }
+
+    public :
+    const char* getClassName(){
+        return this-> name;
     }
 };
 
--- src/Services.ino
+++ src/Services.ino
@@ -5,11 +5,17 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <malloc.h>
+
+#include "Course.ino"
 class Services
 {
 private:
+    const char *NO_CLASS_MESSAGE = "No ongoing class found";
+    const int NUM_CLASSES = 3;
     const char *ssid = "";
     const char *password = "";
+    static Course *classes;
 
 public:
     Services(const char *ssid, const char *password)
@@ -17,8 +23,24 @@ public:
         this->ssid = ssid;
         this->password = password;
 
+        classes = (Course *)malloc(NUM_CLASSES * sizeof(Course));
+
+        memcpy(this->classes, getRoomClasses(), sizeof(Course) * NUM_CLASSES);
         connectToNetWork(ssid, password);
     };
+
+private:
+    Course *getRoomClasses()
+    {
+        static Course roomClasses[3] =
+            {
+                Course("Mobile", 8, 11, 4),
+                Course("IOT", 15, 18, 2),
+                Course("POO2", 8, 11, 1)
+            };
+
+        return roomClasses;
+    }
 
 private:
     void connectToNetWork(const char *ssid, const char *password)
@@ -42,20 +64,25 @@ public:
         PubSubClient client(wifiClient);
         const char *domain = "broker.emqx.io";
         const char *topic = getTopic();
+
         while (!client.connected())
         {
             client.setServer(domain, 1883);
-            Serial.println("Attempting Connection");
             if (client.connect("C1201_Card_Scanner"))
             {
-                client.subscribe(topic);
-                Serial.println("Connected");
-                // client.setCallback(getPayload); The function to call
+                if (topic != NO_CLASS_MESSAGE)
+                {
+                    client.subscribe(topic);
+                    Serial.println("Connected");
+                    // client.setCallback(getPayload); The function to call
+                }
+                else
+                {
+                    // TODO : RED LIGHT
+                }
             }
             else
             {
-                Serial.println("Not Connected :(");
-                //
                 delay(3000);
             }
         }
@@ -64,33 +91,52 @@ public:
 private:
     const char *getTopic()
     {
-        //{"Sunday = 0", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-        int weekday = getWeekDay();
-        const char *ongoingClass = getOngoingClass();
+        for (int i = 0; i < NUM_CLASSES; i++)
+        {
+            if (isOngoingClass(i))
+            {
+                static char buffer[256];
+                sprintf(buffer, "C1201/%S", this->classes[i].getClassName());
+
+                return buffer;
+            }
+        }
+        return this->NO_CLASS_MESSAGE;
+    }
+    bool isOngoingClass(int i)
+    {
+        return isInClassRange(this->classes[i], getCurrentHour()) && isSameDay(this->classes[i], getWeekDay());
     };
 
 private:
-    const char *getOngoingClass()
+    boolean isInClassRange(Course course, int currentHour)
     {
-        // make a system that recognize that this class takes place at this time.
-        // TODO : Make a singleton that will represent the list of courses in that local
+        return course.getStartingHour() <= currentHour && course.getEndHour() > currentHour;
+    };
+
+private:
+    boolean isSameDay(Course course, int weekDay)
+    {
+        return weekDay == course.getWeekDay();
     }
 
 private:
     int getWeekDay()
     {
+        //{"Sunday = 0", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
         time_t currentTime = time(0);
         tm *now = localtime(&currentTime);
 
         return now->tm_wday;
-    }
+    };
 
 private:
-    int getCurrentHour(){
+    int getCurrentHour()
+    {
         time_t currentTime = time(0);
         tm *now = localtime(&currentTime);
-        
-        return now->tm_hour; 
-    }
+
+        return now->tm_hour;
+    };
 };
 #endif
